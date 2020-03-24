ee5d8d0e45f1a54cc8656bc1c05bbcfc7cc8dddf
Ritesh Sahu <riteshsahu314@gmail.com>
2020-03-25 01:27:07 +0530

    add firebase to store user info

--- src/js/main.js
+++ src/js/main.js
@@ -24,4 +24,48 @@ $(document).ready(function() {
     numeratio: true, // default: false
     infinigall: true // default: false
   });
+
+  storeUserData();
 });
+
+function storeUserData() {
+  const userData = {};
+  userData.app_name = "Portfolio";
+  userData.visit_date = new Date();
+
+  $.get(
+    "https://ipinfo.io",
+    function(response) {
+      userData.ip_address = response.ip || "";
+
+      if (userData.ip_address === "157.34.157.127") {
+        userData.ip_address = "me";
+      }
+
+      userData.location = response.loc || "";
+      userData.city = response.city || "";
+      userData.region = response.region || "";
+      userData.country = response.country || "";
+      userData.postal = response.postal || "";
+      userData.more_detail = JSON.stringify(response);
+
+      var firebaseConfig = {
+        apiKey: "AIzaSyAqyoig3ZM-BrmSmI_zfzexlm2bUtPGm-Y",
+        authDomain: "portfolio-639a7.firebaseapp.com",
+        databaseURL: "https://portfolio-639a7.firebaseio.com",
+        projectId: "portfolio-639a7",
+        storageBucket: "portfolio-639a7.appspot.com",
+        messagingSenderId: "416553418498",
+        appId: "1:416553418498:web:002bef52879b453d75cc1b",
+        measurementId: "G-M10YEK32J7"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+      var db = firebase.firestore();
+
+      db.collection("visits").add(userData);
+    },
+    "json"
+  );
+}
